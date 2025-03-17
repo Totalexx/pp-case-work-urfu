9e4ac8adfcd16b52e52174a5c9321bc6d6c28a72
Vitaliy Popov <0702vitaliy@gmail.com>
2025-03-17 20:26:49 +0500

    Начальный коммит

---
+++ service-backend/src/test/java/ru/totalexx/work_service/WorkServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.totalexx.work_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
