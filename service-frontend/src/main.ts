70537bf2234a31b95145e6d0a0147d8bb452e286
Vitaliy Popov <0702vitaliy@gmail.com>
2025-03-19 13:00:45 +0500

    Добавлен Router и Api

--- service-frontend/src/main.ts
+++ service-frontend/src/main.ts
@@ -3,4 +3,10 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.config.compilerOptions.comments = false
+app.config.globalProperties.backendURL = 'http://localhost:8081'
+
+app.use(router)
+app.mount("#app")
